44821a7cbee49461d772465824bd9742beee1add
Shiran Kumarasingha <shirankumarasingha97@gmail.com>
2024-09-18 15:46:44 +0530

    collecting user input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,6 @@
-export default function Input({label,textarea,...props}){
+import { forwardRef } from "react";
+
+const Input =  forwardRef( function Input({label,textarea,...props},ref){
 
 
     const classes = "w-full border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
@@ -6,7 +8,10 @@ export default function Input({label,textarea,...props}){
     return (
         <p className="flex flex-col gap-1 my-4">
             <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
-            {textarea ? <textarea className={classes} {...props}/> : <input className={classes} {...props}/>}
+            {textarea ? <textarea ref={ref} className={classes} {...props}/> : <input ref={ref} className={classes} {...props}/>}
         </p>
     );
-}
+});
+
+
+export default Input;
